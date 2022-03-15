6f4ce03518520c7a657c079d3f84f748e6e56ebe
iqny <2728768646@qq.com>
2022-03-15 11:09:06 +0800

    生产者创建队列

--- pkg/rabbitmq/sender.go
+++ pkg/rabbitmq/sender.go
@@ -15,7 +15,9 @@ type Sender struct {
 	Msg       []byte //消息体,回调时使用
 	addr      string
 	exchange  string
-	mx sync.Mutex
+	mx        sync.Mutex
+	queue     amqp.Queue
+	ch        *amqp.Channel
 }
 
 func NweSender(addr, exchange string) *Sender {
@@ -30,23 +32,26 @@ func NweSender(addr, exchange string) *Sender {
 	return &s
 }
 
-func (s *Sender) Send(name, job string, sendMsg interface{})(err error) {
+func (s *Sender) Send(name, job string, sendMsg interface{}) (err error) {
 	//连接断开了，进行重新连接
 	s.mx.Lock()
 	if s.conn == nil || s.conn.IsClosed() {
 		err = s.connect()
 		s.mx.Unlock()
-		if err!=nil{
+		if err != nil {
 			return err
 		}
-	}else{
+	} else {
 		s.mx.Unlock()
 	}
 	s.QueueName = name
 	s.Job = job
-	s.Msg,err = byteEncoder(sendMsg)
+	s.Msg, err = byteEncoder(sendMsg)
 	if err != nil {
 		return err
+	}
+	if err = s.queueDeclare(); err != nil {
+		return
 	}
 	var encResult bytes.Buffer
 	enc := gob.NewEncoder(&encResult)
@@ -54,13 +59,74 @@ func (s *Sender) Send(name, job string, sendMsg interface{})(err error) {
 		return err
 	}
 
-	ch, err := s.conn.Channel()
+	err = s.ch.Publish(
+		s.exchange,   // exchange
+		s.queue.Name, // routing key
+		false,        // mandatory
+		false,        // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        encResult.Bytes(),
+		})
+	//log.Printf(" [x] Sent %s", enc_result.Bytes())
+	return err
+}
+
+func (s *Sender) connect() (err error) {
+	s.conn, err = amqp.Dial(s.addr)
 	if err != nil {
-		return err
+		return
+	}
+	if err = s.channel();err!=nil{
+		return
 	}
 
-	defer ch.Close()
-	err = ch.ExchangeDeclare(
+	if err = s.exchangeDeclare();err!=nil{
+		return
+	}
+
+
+	return
+}
+func (s *Sender) Close() {
+	err := s.conn.Close()
+	if err != nil {
+		return 
+	}
+	s.ch.Close()
+}
+func byteEncoder(s interface{}) ([]byte, error) {
+	var result bytes.Buffer
+	enc := gob.NewEncoder(&result)
+	if err := enc.Encode(s); err != nil {
+		return nil, err
+	}
+	return result.Bytes(), nil
+}
+func (s *Sender) queueDeclare() (err error) {
+	s.queue, err = s.ch.QueueDeclare(
+		s.QueueName, // name
+		true,        // durable //是否持久化
+		false,       // delete when unused
+		false,       // exclusive
+		false,       // no-wait
+		nil,         // arguments
+	)
+	if err != nil {
+		return
+	}
+	err = s.ch.QueueBind(
+		s.queue.Name,
+		//在pub/sub模式下，这里的key要为空
+		s.queue.Name,
+		s.exchange,
+		false,
+		nil,
+	)
+	return
+}
+func (s *Sender) exchangeDeclare() (err error) {
+	err = s.ch.ExchangeDeclare(
 		//交换机名称
 		s.exchange,
 		//交换机类型 广播类型
@@ -75,46 +141,12 @@ func (s *Sender) Send(name, job string, sendMsg interface{})(err error) {
 		false,
 		nil,
 	)
+	return
+}
+func (s *Sender) channel() (err error) {
+	s.ch, err = s.conn.Channel()
 	if err != nil {
 		return err
 	}
-	q, err := ch.QueueDeclare(
-		s.QueueName, // name
-		true,        // durable //是否持久化
-		false,       // delete when unused
-		false,       // exclusive
-		false,       // no-wait
-		nil,         // arguments
-	)
-	if err != nil {
-		return err
-	}
-	err = ch.Publish(
-		s.exchange, // exchange
-		q.Name,     // routing key
-		false,      // mandatory
-		false,      // immediate
-		amqp.Publishing{
-			ContentType: "text/plain",
-			Body:        encResult.Bytes(),
-		})
-	//log.Printf(" [x] Sent %s", enc_result.Bytes())
-	return err
-}
-
-func (s *Sender) connect() error {
-	var err error
-	s.conn, err = amqp.Dial(s.addr)
-	return err
-}
-func (s *Sender) Close() {
-	s.conn.Close()
-}
-func byteEncoder(s interface{}) ([]byte,error) {
-	var result bytes.Buffer
-	enc := gob.NewEncoder(&result)
-	if err := enc.Encode(s); err != nil {
-		return nil, err
-	}
-	return result.Bytes(),nil
+	return
 }
